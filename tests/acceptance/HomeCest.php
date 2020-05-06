5657c9cc81a18962066af7eb9e690faac54a50a0
Tigran Ghabuzyan <tigran.ghabuzyan@gmail.com>
2020-05-06 11:35:09 +0400

    Prepared Read.me dock, cleaned up.

--- tests/acceptance/HomeCest.php
+++ tests/acceptance/HomeCest.php
@@ -7,12 +7,12 @@ class HomeCest
     public function ensureThatHomePageWorks(AcceptanceTester $I)
     {
         $I->amOnPage(Url::toRoute('/site/index'));        
-        $I->see('My Company');
+        $I->see('Local Express Imports');
         
-        $I->seeLink('About');
-        $I->click('About');
+        $I->seeLink('Add Import');
+        $I->click('Add Import');
         $I->wait(2); // wait for page to be opened
         
-        $I->see('This is the About page.');
+        $I->see('Import Products');
     }
 }
